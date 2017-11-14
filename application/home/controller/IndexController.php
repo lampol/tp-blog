e396fd323d9d49322570949eee2bb6a52342342f
lampol <807968192@qq.com>
2017-11-15 02:21:21 +0800

    第27个视频

--- application/home/controller/IndexController.php
+++ application/home/controller/IndexController.php
@@ -2,12 +2,16 @@
 namespace app\home\controller;
 
 use think\Controller;
+use app\home\model\Article;
 
 class IndexController extends Controller
 {
 
 	public function index(){
-	
+
+		$article = new Article;
+		$articles = $article->getAllArticle();
+		$this->assign('articles',$articles);
 		return $this->fetch();
 	}
 }
--- application/home/model/Article.php
+++ application/home/model/Article.php
@@ -5,5 +5,9 @@ use think\Model;
 
 class Article extends Model
 {
-
+	public function getAllArticle(){
+		$data = $this->field(['id','title','author','img_url','summary','tag','created_at','views'])->paginate(3);
+		return $data;
+	
+	}
 }
