69f8321fcf588e6a2e16115120955b56dbeb68d0
lampol <807968192@qq.com>
2017-11-30 00:07:48 +0800

    第47个视频

--- application/home/controller/SearchController.php
+++ application/home/controller/SearchController.php
@@ -5,12 +5,32 @@ namespace app\home\controller;
 use think\Controller;
 use think\Request;
 use app\home\controller\BaseController;
+use lampol\SphinxClient;
+
 
 class SearchController extends BaseController
 {
 
 	public function search(Request $request){
-		dump($request->param('words'));
+		$sc = new SphinxClient ();
+		$words = $request->param('words');
+		$host = "localhost";
+		$port = 9312;
+		$index = "article";
+		$sc->SetServer ( $host, $port );
+
+		//设置连接的超时时间
+
+		$sc->SetConnectTimeout ( 1 );
+
+		//返回数组的数据格式
+		$sc->SetArrayResult ( true );
+		//开始查询
+		$res = $sc->Query ( $words, $index );
+
+		echo '<pre>';
+		dump($res);
+		echo '</pre>';	
 	
 	}
 }
